33a5dad0622ad63e9cf2c3e0635110d52a2cb5b5
Munchi <camaragongorajoseabraham@gmail.com>
2026-07-24 12:31:37 -0600

    fix: change base path to relative for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: '/K-iinil/',
+    base: './',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
